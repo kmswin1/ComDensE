2fe61a8e4a5269c790aa18442a8f49a24eed0783
lucas.ai <lucas.ai@kakaoenterprise.com>
2021-09-12 14:37:54 +0900

    debugging

--- model.py
+++ model.py
@@ -24,7 +24,7 @@ class DensE(nn.Module):
         self.bceloss = torch.nn.BCELoss()
 
         for w in self.ww:
-            torch.nn.init.xavier_normal_(w)
+            torch.nn.init.xavier_normal_(w.weight)
         torch.nn.init.xavier_normal_(self.ent_emb.data)
         torch.nn.init.xavier_normal_(self.rel_emb.data)
         torch.nn.init.xavier_normal_(self.w_r0.data)
